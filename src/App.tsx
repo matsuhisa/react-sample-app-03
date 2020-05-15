28a3685f9c00f5874005582daf752a0aa492056c
matsuhisa <matsuhisa+github@gmail.com>
2020-05-16 01:18:15 +0900

    Add TaskList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import TaskList from './TaskList'
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <TaskList />
+    </>
   );
 }
 
---
+++ src/TaskList.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+interface TaskListPorps {
+}
+
+const TaskList: FC<TaskListPorps> = ({}) => (
+ <>
+  <div>タスクリスト</div>
+ </> 
+)
+
+export default TaskList
