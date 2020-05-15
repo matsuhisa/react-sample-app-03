ffec4e053afba12e010be035a7b4e1f894b5f814
matsuhisa <matsuhisa+github@gmail.com>
2020-05-16 01:18:41 +0900

    Add TaskList Stories

---
+++ src/stories/2-TaskList.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import TaskList from '../TaskList'
+
+export default {
+  title: 'TaskList',
+  component: TaskList,
+}
+
+export const List = () => (
+  <TaskList></TaskList>
+)
